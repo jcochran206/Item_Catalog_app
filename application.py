e9664212a005806051c273466b14322a6892a7e4
jcochran206 <jonathan.o.cochran@gmail.com>
2018-01-04 23:58:37 -0800

    connection string
    added connection to database

--- application.py
+++ application.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import User, Category, Item
 app = Flask(__name__)
+
+engine = create_engine('sqlite:///categoryitems.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 @app.route('/index')
