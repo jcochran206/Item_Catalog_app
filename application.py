60372878fafbb18c1d99a991e07731a4f803c2f9
jcochran206 <jonathan.o.cochran@gmail.com>
2018-01-08 22:32:14 -0800

    stood up virtual server
    updated application routing to include json api

--- application.py
+++ application.py
@@ -15,6 +15,30 @@ session = DBSession()
 def home():
 	return "hello index"
 
+@app.route('/Category/JSON')
+def categoryJSON():
+	#return all categories
+	categories = session.query(Category).all()
+	return jsonify(categories=[i.serialize for i in categories])
+
+#show all categories
+@app.route('/categories/')
+def showCategories():
+	categories = session.query(Category).order_by(asc(Category.name))
+	return "get out put here to render"
+
+#NEW CATEGORY
+@app.route('/categories/new/' methods=['GET','POST'])
+def newCategory():
+	return 'New item here'
+
+#Edit Category here
+@app.route('/categories/<int:category_id>/edit/', methods=['GET','POST'])
+def editCategory(category_id)
+	return 'edit materials go here'
+
+
+
 if __name__ == '__main__':
 	app.debug = True
 	app.run(localhost = '0.0.0.0', port=8000)
